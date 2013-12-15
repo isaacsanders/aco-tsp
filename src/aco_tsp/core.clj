2c1ad2c641451e869e8eb286d9e94b33346dd623
Robbie Cooper <cooperra@rose-hulman.edu>
2013-12-15 16:49:11 -0500

    Move pheromone code from graph to core

--- src/aco_tsp/core.clj
+++ src/aco_tsp/core.clj
@@ -4,3 +4,20 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn file->graph [f]
+  f)
+
+(defn (init-pheromones [g]
+		       (reduce (fn [m elem] (assoc m elem 0 ))
+			       {}
+			       (edges g))))
+
+(defn (add-pheromone [amt edge m]
+		     (update-in m [edge] (partial + amt))))
+
+(defn (decay-one-pheromone [decay-fn edge m]
+			   (update-in m [edge] (comp (partial max 0) decay-fn))))
+
+(defn (decay-pheromones [decay-fn m]
+			(update-in m (keys m) (comp (partial max 0) decay-fn))))

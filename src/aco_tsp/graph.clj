19052655da7446ab004e06b79653c18545503bee
Robbie Cooper <cooperra@rose-hulman.edu>
2013-12-15 16:39:49 -0500

    Implement tested functions (tests not run yet)

--- src/aco_tsp/graph.clj
+++ src/aco_tsp/graph.clj
@@ -3,3 +3,17 @@
 
 (defn file->graph [f]
   f)
+
+(defn (init-pheromones [g]
+		       (reduce (fn [m elem] (assoc m elem 0 ))
+			       {}
+			       (edges g))))
+
+(defn (add-pheromone [amt edge m]
+		     (update-in m [edge] (partial + amt))))
+
+(defn (decay-one-pheromone [decay-fn edge m]
+			   (update-in m [edge] (comp (partial max 0) decay-fn))))
+
+(defn (decay-pheromones [decay-fn m]
+			(update-in m (keys m) (comp (partial max 0) decay-fn))))

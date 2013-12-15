90fa28c99f391192d1fd9eadc8b4c6b8e2027345
Robbie Cooper <cooperra@rose-hulman.edu>
2013-12-15 17:27:43 -0500

    Fix syntax errors and decay-pheromones bug

--- src/aco_tsp/core.clj
+++ src/aco_tsp/core.clj
@@ -8,16 +8,17 @@
 (defn file->graph [f]
   f)
 
-(defn (init-pheromones [g]
-		       (reduce (fn [m elem] (assoc m elem 0 ))
+(defn init-pheromones [g]
+      		      (reduce (fn [m elem] (assoc m elem 0 ))
 			       {}
-			       (edges g))))
+			       (edges g)))
 
-(defn (add-pheromone [amt edge m]
-		     (update-in m [edge] (partial + amt))))
+(defn add-pheromone [amt edge m]
+      		    (update-in m [edge] (partial + amt)))
 
-(defn (decay-one-pheromone [decay-fn edge m]
-			   (update-in m [edge] (comp (partial max 0) decay-fn))))
+(defn decay-one-pheromone [decay-fn edge m]
+			  (update-in m [edge] (comp (partial max 0) decay-fn)))
 
-(defn (decay-pheromones [decay-fn m]
-			(update-in m (keys m) (comp (partial max 0) decay-fn))))
+(defn decay-pheromones [decay-fn m]
+		       (reduce (fn [m e]
+		       	       	   (update-in m [e] (comp (partial max 0) decay-fn))) m (keys m)))

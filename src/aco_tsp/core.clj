43ee4dc267e4cdeeee72aa73d7cfd52eed4d5f95
Robbie Cooper <cooperra@rose-hulman.edu>
2013-12-16 16:33:57 -0500

    Moved constants into a mapping

--- src/aco_tsp/core.clj
+++ src/aco_tsp/core.clj
@@ -20,8 +20,9 @@
   (* (p i j)
      (expt (visibility g i j) beta)))
 
-(defn choose-next-city [graph pheromones current previous beta cl q-sub-0]
-  (let [candidates (take cl (sort-by #(weight graph current %)
+(defn choose-next-city [graph pheromones current previous constants]
+  (let [beta (constants :beta)
+	candidates (take (constants :cl) (sort-by #(weight graph current %)
                                      (clojure.set/difference (set (successors graph current))
                                                              (set previous))))
         pheromones-fn (fn [candidate] (tau-eta graph pheromones current candidate beta))
@@ -45,30 +46,30 @@
                          (chance-fn (probs-fn {} candidates)))]
     (if (empty? candidates)
       (apply (partial max-key (partial weight graph current)) (successors graph current))
-      (if (<= (rand) q-sub-0)
+      (if (<= (rand) (constants :q-sub-0))
         (apply (partial max-key pheromones-fn) candidates)
         (probability-fn candidates)))))
 
 ; returns chosen tour
-(defn next-step [[current tour] graph pheromones beta cl q-sub-0]
+(defn next-step [[current tour] graph pheromones constants]
   (let [unvisited (clojure.set/difference (set (nodes graph)) (set tour))]
     (case
       (nil? current) [current tour]
       (and (= current (first tour)) (empty? unvisited)) [nil (concat tour [current])]
-      :else (let [next-state (choose-next-city graph pheromones current tour beta cl q-sub-0)]
+      :else (let [next-state (choose-next-city graph pheromones current tour constants)]
               [next-state (concat tour [current])]))))
 
-(defn do-transition [graph ants pheromones beta cl q-sub-0]
-  (let [next-ants (map (fn [ant] (next-step ant graph pheromones beta cl q-sub-0)) ants)
+(defn do-transition [graph ants pheromones constants]
+  (let [next-ants (map (fn [ant] (next-step ant graph pheromones constants)) ants)
         tours (map second next-ants)
         new-pheromones (decay-pheromones pheromones (vec (zipmap (filter (comp not nil?) (map first ants))
                                                             (filter (comp not nil?) (map first next-ants)))))]
     [ants pheromones]))
 
-(defn find-tours [graph ants pheromones beta cl q-sub-0]
+(defn find-tours [graph ants pheromones constants]
   (if (every? #(nil? (first %)) ants)
     [ants pheromones]
-    (let [[ants pheromones] (do-transition graph ants pheromones beta cl q-sub-0)]
+    (let [[ants pheromones] (do-transition graph ants pheromones constants)]
       (recur graph ants pheromones))))
 
 (defn tour-edges [node-list]
@@ -83,18 +84,18 @@
                                         (* rho (/ 1 (tour-cost tour))))))
           pheromones (tour-edges tour)))
 
-(defn solve [graph antcount init-ants-fn init-pheromones-fn beta rho cl q-sub-0]
-  (let [ants (init-ants-fn graph antcount)]
+(defn solve [graph init-ants-fn init-pheromones-fn constants]
+  (let [ants (init-ants-fn graph (constants :antcount))]
     (loop [time-step 0
            best-tour nil
            pheromones (init-pheromones-fn graph)]
       (if (> time-step 100)
         (list best-tour pheromones) ; return
-        (let [[new-ants new-pheromones] (find-tours graph ants pheromones beta cl q-sub-0)]
+        (let [[new-ants new-pheromones] (find-tours graph ants pheromones constants)]
           (recur (inc time-step) (apply (partial min-key
                                                  (partial tour-cost graph))
                                         (map second new-ants))
-                 (update-pheromones new-pheromones best-tour rho)))))))
+                 (update-pheromones new-pheromones best-tour (constants :rho))))))))
 
 ; p^k[i,j](t)
 ; i : node
@@ -103,15 +104,17 @@
 ; pheromones : (node node) -> int
 ; sight : (node node) -> int
 ; returns : probability of choosing node j as the next node
-(defn as-transition-rule [i j unvisited pheromones sight alpha beta]
-  (/ (* (expt (pheromones i j) alpha)
-        (expt (sight i j) beta))
-        (apply + (map (fn [unvisited-node] (* (expt (pheromones i unvisited-node) alpha)
-                                              (expt (sight i unvisited-node) beta)))
-                      unvisited))))
+(defn as-transition-rule [i j unvisited pheromones sight constants]
+  (let [alpha (constants :alpha)
+	beta (constants :beta)]
+    (/ (* (expt (pheromones i j) alpha)
+	  (expt (sight i j) beta))
+       (apply + (map (fn [unvisited-node] (* (expt (pheromones i unvisited-node) alpha)
+					     (expt (sight i unvisited-node) beta)))
+		     unvisited)))))
 
-(defn aco-transition-rule [i j unvisited pheromones sight beta]
-  (as-transition-rule i j unvisited pheromones sight 1 beta))
+(defn aco-transition-rule [i j unvisited pheromones sight constants]
+  (as-transition-rule i j unvisited pheromones sight 1 (constants beta)))
 
 (defn aco-init-ants-fn [graph antcount]
   (map (fn [i] [i []]) (take antcount (shuffle (nodes graph)))))
@@ -122,14 +125,19 @@
 
 (defn -main [filename antcount beta rho cl q-sub-0]
   (let [cities (file->graph (file filename))
-        antcount (Integer/parseInt antcount)]
-    (let [[best-tour pheromones] (solve cities antcount
+        antcount (Integer/parseInt antcount)
+        cl (Integer/parseInt cl)
+        rho (Float/parseFloat rho)
+        beta (Float/parseFloat beta)
+        q-sub-0 (Float/parseFloat q-sub-0)]
+    (let [[best-tour pheromones] (solve cities
 					aco-init-ants-fn
 					aco-init-pheromones-fn
-					beta
-					rho
-					cl
-					q-sub-0)]
+					{:antcount antcount,
+					 :beta beta,
+					 :rho rho,
+					 :cl cl,
+					 :q-sub-0 q-sub-0})]
       )))
 
 ;(defn change-pheromones [best-tour tours pheromones]

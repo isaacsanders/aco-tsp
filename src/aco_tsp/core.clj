9e4a89952aef7e5deb9e4ed39a6f949ad887c124
Robbie Cooper <cooperra@rose-hulman.edu>
2013-12-15 21:01:24 -0500

    things

--- src/aco_tsp/core.clj
+++ src/aco_tsp/core.clj
@@ -15,3 +15,46 @@
 (defn decay-pheromones [decay-fn m]
   (reduce (fn [m e]
             (update-in m [e] (comp (partial max 0) decay-fn))) m (keys m)))
+
+(defn solve [graph antcount init-ants-fn init-pheromone-fn]
+  (let [ants (init-ants-fn graph antcount)]
+       [pheromone (init-pheromone-fn graph)]
+       (loop [time 0
+	      ants ants
+	      pheromone pheromone]
+	     )))
+
+; returns chosen tour
+(defn ant-tour [start graph pheromones transition-fn]
+      (let [ant-tour-helper (fn [current visited unvisited traversed-edges]
+				(case [(and (= current start)
+					    (empty? unvisited))
+					    visited]
+				      [else (let [next (transition-fn current visited graph pheromones)]
+						 (ant-tour-helper next
+								  (append visited (list next))
+								  (disj unvisited next))]))]
+	   (ant-tour-helper start (list start) (nodes graph) (list))))
+
+; p^k[i,j](t)
+; i : node
+; j : node
+; unvisited : set of node
+; pheromones : (node node) -> int
+; sight : (node node) -> int
+(defn as-transition-rule [i j unvisited pheromones sight alpha beta]
+  (/ (* (exp (pheromones i j) alpha)
+	(exp (sight i j) beta)
+     (apply + (map (fn [unvisited-node] (* (exp (pheromones i unvisited-node) alpha)
+				     (exp (sight i unvisited-node) beta)))
+	       unvisited)))))
+
+(defn acs-transition-rule [i j unvisited pheromones sight beta]
+  (as-transition-rule i j unvisited pheromones sight 1 beta))
+
+(defn init-sight [graph]
+  (let [sight (fn [i j]
+		  (/ 1
+		     (weight graph i j)))]
+    sight))
+
